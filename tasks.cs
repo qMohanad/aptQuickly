44368361be11eac8ef321508cd44e009d8256912
qMohanad <76506835+qMohanad@users.noreply.github.com>
2024-06-08 11:28:52 +0300

    Add files via upload

---
+++ tasks.cs
@@ -0,0 +1,12 @@
+[DEVELOPMENTS]:
+    [#] Add Dark Mode / Light Mode
+    [#] Filter System [Price , Rooms , tags.]
+[•] Apartments Page:
+    [A] show available Apartments
+    [B] add Likes List
+    [C] show details for each Apartment and room
+[••] Admins Page:
+    [A] Order system [ name, phone number, gender ]
+    [B] Orders List -> admins can read each order's information
+    [C] Admins should contact with the customer by phone or whatsapp chat
+    [D] Accept Orders -> Assign the room has +1 -> ex. 4/6 Slots
